44e5325734abb3c66eca7629a9505a3e031f4b20
buttieric <eric_butti@hotmail.com>
2015-07-06 04:20:51 +0200

    commit

--- dice.R
+++ dice.R
@@ -1,12 +1,31 @@
-#simulation script class
+#dice simulation
 
-d = 100
+d = 1000
 
 dice1 <- sample(6,d,replace=TRUE)
 dice2 <- sample(6,d,replace=TRUE)
 
 
+
+
 rolling <- dice1 + dice2
 
 
+#histogramms
 hist(rolling)
+hist(rolling, freq=FALSE)
+
+
+#cummulative distribution
+plot(ecdf(rolling))
+
+
+#normal distribution
+n <- rnorm(1000, mean=5, sd=10)
+hist(n)
+
+
+
+
+z <- (68-70)/3.3
+
